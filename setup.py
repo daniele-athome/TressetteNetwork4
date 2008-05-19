d27c17078df772223c28e4934bec283fe14462e1
Daniele Ricci <daniele.athome@gmail.com>
2008-05-19 21:14:43 +0000

    Completed setup script for Linux, to be tested on Windows Added license file

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@
 from distutils.core import setup
 import main
 
-setup(name=main.PACKAGE,
-	version=main.VERSION,
-	description=main.NAME,
-	author=main.AUTHORS[0]['name'],
-	author_email=main.AUTHORS[0]['email'],
-	url='http://gna.org/projects/tsnet4',
-	packages=['','netframework'],
-	scripts=['tressettenetwork4', 'ts4launcher']
+setup(	name		= main.PACKAGE,
+		version		= main.VERSION,
+		description	= main.NAME,
+		author		= main.AUTHORS[0]['name'],
+		author_email= main.AUTHORS[0]['email'],
+		url			= 'http://gna.org/projects/tsnet4',
+		packages	= ['','netframework'],
+		scripts		= ['tressettenetwork4', 'ts4launcher'],
+		license		= 'GNU GPL v2',
+		requires	= ['wx'],
+		package_data= { '': ['cards/piacentine*.jpg'] }
 )
