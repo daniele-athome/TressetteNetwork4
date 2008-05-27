3a7660a0b82239d6b478be35c4b0cb0a11d8b344
Daniele Ricci <daniele.athome@gmail.com>
2008-05-27 21:38:26 +0000

    Release candidate 4! TODO upgrade

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ import netframework
 # version information
 NAME="TressetteNetwork4"
 PACKAGE=NAME.lower()
-VERSION="0.0.1-rc4"
+VERSION="0.0.1-rc5"
 AUTHORS=( {"name": "Daniele Ricci", "email" : "daniele.athome@gmail.com"}, )
 
 
