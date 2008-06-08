fe559a33bf79af4f54a87b40f55a1c99069edf71
Daniele Ricci <daniele.athome@gmail.com>
2008-06-08 09:58:57 +0000

    First stable revision of metaserver script

--- metaserver/metaserver.php
+++ metaserver/metaserver.php
@@ -25,14 +25,20 @@ $config['table'] = 'servers';
 
 $config['default-port'] = 8154;
 
+define(PACKAGE,"TSNet4 Metaserver");
+define(VERSION,"0.0.1-rc1");
+
 # stampa l'header della pagina
-function template_header() {
+function template_header($name='',$host='',$port='',$desc='') {
 	print <<<EOT
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
 <head>
-<title>TressetteNetwork4 Metaserver</title>
+EOT;
+
+?>
+<title><?php print PACKAGE." versione ".VERSION; ?></title>
 </head>
 
 <body bgcolor="green" style="color: white; font-family: Verdana,Arial;" link="white" alink="white" vlink="white">
@@ -43,19 +49,18 @@ function template_header() {
 
 <div align="center">
 <form method="get" action="{$_SERVER['PHP_SELF']}">
-<b>Nome:</b> <input type="text" name="name"/>
-<b>Indirizzo:</b> <input type="text" name="host"/>
-<b>Porta:</b> <input type="text" size="5" name="port"/>
-<b>Descrizione:</b> <input type="text" name="desc"/>
-<input type="hidden" name="op" value="list"/>
+<b>Nome:</b> <input type="text" name="name" value="<?php print $name; ?>"/>
+<b>Indirizzo:</b> <input type="text" name="host" value="<?php print $host; ?>"/>
+<b>Porta:</b> <input type="text" size="5" name="port" value="<?php print $port; ?>"/>
+<b>Descrizione:</b> <input type="text" name="desc"  value="<?php print $desc; ?>"/>
+<input type="hidden" name="op" value="list"  value="{$_GET['list']}"/>
 <input type="submit" value="Filtra"/>
 </form>
 </div>
 
 <hr/>
 
-
-EOT;
+<?
 }
 
 # stampa il footer della pagina
@@ -193,29 +198,55 @@ if (array_key_exists('op',$_GET)) {
 		$name = return_has_key($_GET,'name');
 		$host = return_has_key($_GET,'host');
 		$port = return_has_key($_GET,'port');
+
+		if ($name == null or $host == null) {
+			print 'EARGS';
+			exit;
+		}
+
 		if ($port == null) {
 			$port = $config['default-port'];
 		}
+
+		elseif (!is_numeric($port) or (intval($port) < 1 or intval($port) > 65536)) {
+			print 'EPORT';
+			exit;
+		}
+
 		$desc = return_has_key($_GET,'desc');
 		print register_server($db,$config['table'],$host,$port,$name,$desc);
 	}
 
 	elseif ($_GET['op'] == 'list') {
-		template_header();
 		$name = return_has_key($_GET,'name',true);
 		$host = return_has_key($_GET,'host',true);
-		$port = intval(return_has_key($_GET,'port',true));
+		$port_s = return_has_key($_GET,'port',true);
+		$port = intval($port_s);
+
+		if (!is_numeric($port_s) or ($port < 1 or $port > 65536)) {
+			$port_s = '';
+			$port = null;
+		}
+
 		$desc = return_has_key($_GET,'desc',true);
+
+		template_header($name,$host,$port_s,$desc);
+
 		$tot = get_server_list($db,$config['table'],$host,$port,$name,$desc);
+
 		if (count($tot) == 0) {
 			template_no_results();
+
 		} else {
 			template_table_header();
+
 			foreach ($tot as $val) {
 				template_entry($val['host'],$val['port'],$val['name'],$val['desc']);
 			}
+
 			template_table_footer();
 		}
+
 		template_footer();
 	}
 }
